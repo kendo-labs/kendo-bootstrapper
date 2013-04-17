4e7274b984fd9126488bf0c3f9c38f9c7182e068
Mihai Bazon <mihai.bazon@gmail.com>
2013-04-17 13:45:41 +0200

    Try to better guess the editor window title so that it runs in foreground.

--- os/win/run-in-foreground.js
+++ os/win/run-in-foreground.js
@@ -15,4 +15,20 @@ var cmd = quote(app) + " " + args.join(" ");
 
 shell.run(cmd);
 WScript.Sleep(200);
-shell.AppActivate(app);
+
+// attempt to activate the app.
+
+// MSDN about Shell.AppActivate:
+//
+//     "In determining which application to activate, the specified
+//     title is compared to the title string of each running
+//     application. If no exact match exists, any application whose
+//     title string begins with title is activated. If an application
+//     still cannot be found, any application whose title string ends
+//     with title is activated. If more than one instance of the
+//     application named by title exists, one instance is arbitrarily
+//     activated."
+
+// taking a wild guess about the *title* of the window here:
+var exename = app.replace(/^.*[\\\/]([^\\\/]+)$/, "$1").replace(/\.[^.]*$/, "");
+shell.AppActivate(exename);
